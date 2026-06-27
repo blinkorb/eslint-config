feea819b4524574ea8578e634d11f411dfdb7260
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-06-27 20:46:54 +0100

    Use a reference comment for the eslint-config-expo types

--- src/expo.ts
+++ src/expo.ts
@@ -1,4 +1,5 @@
-import './@types/eslint-config-expo';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./@types/eslint-config-expo.d.ts" />
 
 import { defineConfig } from 'eslint/config';
 import expoConfig from 'eslint-config-expo/flat';
