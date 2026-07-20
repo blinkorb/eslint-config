4db3f2c2864bda7435861910fe9060f96f78d2fb
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-07-20 23:54:56 +0100

    Create script to check that package.json contains exports for all configs

---
+++ scripts/check-exports.ts
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import packageJson from '../package.json';
+
+const EXPORTED_FILES = Object.values(packageJson.exports);
+
+const ROOT_DIR = path.resolve(__dirname, '../');
+const SRC_DIR = path.resolve(ROOT_DIR, './src');
+
+fs.readdirSync(SRC_DIR).forEach((fileOrDirectory) => {
+  const fullPath = path.resolve(SRC_DIR, fileOrDirectory);
+  const relativeToRoot = `./${path.relative(ROOT_DIR, fullPath)}`;
+
+  if (!fs.lstatSync(fullPath).isDirectory()) {
+    if (!EXPORTED_FILES.includes(relativeToRoot)) {
+      throw new Error(
+        `No export for "${relativeToRoot}" in package.json "exports"`
+      );
+    }
+  }
+});
