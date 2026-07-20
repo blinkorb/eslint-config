8571a0d8be3ea044fce07116ba3968d930a4b9f7
Jake 'Sid' Smith <hello@jakesidsmith.com>
2026-07-20 23:56:00 +0100

    Exclude types.ts from exports check

--- scripts/check-exports.ts
+++ scripts/check-exports.ts
@@ -3,12 +3,18 @@ import path from 'node:path';
 
 import packageJson from '../package.json';
 
+const EXCLUDED_FILES = ['types.ts'];
+
 const EXPORTED_FILES = Object.values(packageJson.exports);
 
 const ROOT_DIR = path.resolve(__dirname, '../');
 const SRC_DIR = path.resolve(ROOT_DIR, './src');
 
 fs.readdirSync(SRC_DIR).forEach((fileOrDirectory) => {
+  if (EXCLUDED_FILES.includes(fileOrDirectory)) {
+    return;
+  }
+
   const fullPath = path.resolve(SRC_DIR, fileOrDirectory);
   const relativeToRoot = `./${path.relative(ROOT_DIR, fullPath)}`;
 
